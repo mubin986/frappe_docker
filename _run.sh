f935a69224d24fe506bbf49499968c525db94115
Shariful Islam Mubin <mubin986@gmail.com>
2025-04-16 02:24:23 +0600

    fix: correct sed command for port update in _run.sh

--- _run.sh
+++ _run.sh
@@ -6,7 +6,8 @@ PORT=${1:-8080}
 # Update pwd.yml with the new port
 if [ -f "pwd.yml" ]; then
     # Use sed to replace the port in pwd.yml
-    sed -i '' "s/\"[0-9]*:8080\"/\"${PORT}:8080\"/g" pwd.yml
+    sed -i.bak "s|\"[0-9]*:8080\"|\"${PORT}:8080\"|g" pwd.yml
+    rm -f pwd.yml.bak
 fi
 
 # Run docker compose
